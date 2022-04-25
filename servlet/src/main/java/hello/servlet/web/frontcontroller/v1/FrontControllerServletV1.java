f3da1c13bef9ff703a3fa4dd4212e154f78ff75a
sujin <sujin.lee@kbfg.com>
2022-04-25 17:53:59 +0900

    220425 servlet jsp mvc

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -26,7 +26,6 @@ public class FrontControllerServletV1 extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV1.service");
 
         String requestURI = request.getRequestURI();
 
