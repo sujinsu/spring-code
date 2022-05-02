3cefd96ae85c8fd2effa17fb20d223d921fb51ce
LEE SUJIN <87465326+sujinsu@users.noreply.github.com>
2022-05-02 22:40:55 +0900

    Delete springmvc/springmvc directory

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
+++
@@ -1,9 +0,0 @@
-package hello.springmvc.basic;
-
-import lombok.Data;
-// @Getter , @Setter , @ToString , @EqualsAndHashCode , @RequiredArgsConstructor
-@Data
-public class HelloData {
-    private String username;
-    private int age;
-}
