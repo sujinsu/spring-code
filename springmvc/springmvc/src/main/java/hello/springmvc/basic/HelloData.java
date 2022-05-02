762dc384a4f53d0c9f6e3aec822c149a332c68b0
LEE SUJIN <87465326+sujinsu@users.noreply.github.com>
2022-05-02 22:40:19 +0900

    Add files via upload

---
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,9 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+// @Getter , @Setter , @ToString , @EqualsAndHashCode , @RequiredArgsConstructor
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
