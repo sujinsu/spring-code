e7d121d60cd7b684bbef8e42fd7e69fbaa6b60b9
sujin <whdlsj98@naver.com>
2022-05-19 09:18:18 +0900

    220519

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -7,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Table(name="orders")
+@Getter @Setter
 public class Order {
 
     @Id @GeneratedValue
@@ -24,7 +28,7 @@ public class Order {
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime orderData; // 주문 시간
+    private LocalDateTime orderDate; // 주문 시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCLE]
@@ -44,5 +48,47 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드 ==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for  (OrderItem orderItem: orderItems){
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCLE);
+        for (OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 주문 조회
+     */
+    public int getTotalPrice(){
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : orderItems){
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }
