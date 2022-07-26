a1bd32b13357c06682e7af2f22962646e351fae0
sujin <whdlsj98@naver.com>
2022-07-27 08:51:12 +0900

    thymeleaf 220727

--- mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +46,32 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+        return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data","Spring!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData",null);
+        model.addAttribute("data","Spring!");
+        return "basic/operation";
     }
 
     @Data
