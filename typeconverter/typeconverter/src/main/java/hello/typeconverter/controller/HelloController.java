798fcf85adc78b9b667251547c3d11d7d9575b12
sujin <whdlsj98@naver.com>
2022-11-11 08:46:40 +0900

    221111:

--- typeconverter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.typeconverter.controller;
 
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +26,11 @@ public class HelloController{
         return "ok";
     }
 
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        return "ok";
+    }
 }
